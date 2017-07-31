1ba371a62b8819c81cb266ded6981c08c86a96e2
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2017-07-31 12:48:48 +0100

    testing on server
    code does not run locally

--- js/_vti_cnf/leaflet.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|27 Jun 2017 10:16:26 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|HONEY-POT\\Malcolm
-vti_modifiedby:SR|HONEY-POT\\Malcolm
-vti_timecreated:TR|27 Jun 2017 10:16:26 -0000
-vti_cacheddtm:TX|27 Jun 2017 10:16:26 -0000
-vti_filesize:IR|137140
-vti_backlinkinfo:VX|map/index.html
---
+++ js/map.js
@@ -0,0 +1,28 @@
+//JS for the main map
+
+var map = L.map('map',{
+	center: [5,28],
+	zoom:3,
+	minZoom:2,
+	maxZoom:18
+});
+
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/ianmule.bg2v5cdi/{z}/{x}/{y}.png',{attribution:"Mapbox"}).addTo(map);
+
+//Add some GeoJSON
+
+//var geojsonLayer = new L.GeoJSON.AJAX("../geojson/bristol/exist.geojson");       
+//geojsonLayer.addTo(map);
+
+var district_boundary = new L.geoJson();
+district_boundary.addTo(map);
+
+$.ajax({
+dataType: "json",
+url: "../geojson/bristol/exist.geojson",
+success: function(data) {
+    $(data.features).each(function(key, data) {
+        district_boundary.addData(data);
+    });
+}
+}).error(function() {});

be5dc8bdd48a5abaa51357b852bf484e22bcad0c
Malcolm Morgan <mem48@users.noreply.github.com>
2018-03-19 14:42:41 +0000

    tweak again

--- api/cyiptModel.php
+++ api/cyiptModel.php
@@ -64,36 +64,29 @@ class cyiptModel
 
 			# Max Zoomed Out
 			case ($this->zoom == 11):
-				$fields[] = 'ST_AsGeoJSON(ST_Simplify(geotext, 0.01375)) AS geometry';
-				$limit = 5000;
-				break;
-
-			case ($this->zoom == 12):
 				$fields[] = 'ST_AsGeoJSON(ST_Simplify(geotext, 0.00687)) AS geometry';
 				$limit = 5000;
 				break;
 
-		  case ($this->zoom == 13):
+			case ($this->zoom == 12):
 				$fields[] = 'ST_AsGeoJSON(ST_Simplify(geotext, 0.00344)) AS geometry';
 				$limit = 5000;
 				break;
 
-			case ($this->zoom == 14):
+		  case ($this->zoom == 13):
 				$fields[] = 'ST_AsGeoJSON(ST_Simplify(geotext, 0.00172)) AS geometry';
 				$limit = 5000;
 				break;
 
-			case ($this->zoom == 15):
+			case ($this->zoom == 14):
 				$fields[] = 'ST_AsGeoJSON(ST_Simplify(geotext, 0.00086)) AS geometry';
 				$limit = 5000;
 				break;
 
-			case ($this->zoom >= 16):
+			case ($this->zoom >= 15):
 				$fields[] = 'ST_AsGeoJSON(geotext) AS geometry';
 				$limit = 5000;
 				break;
-
-
 
 		  #Max Zoomed In
 
